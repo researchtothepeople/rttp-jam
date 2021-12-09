2de5060170998139b4276dd386bf3682e7abdd3e
Qinyi Yao <hi@qinyiyao.com>
2021-12-09 15:42:46 -0800

    Catalog results layout & footer

--- packages/web/src/pages/catalog.js
+++ packages/web/src/pages/catalog.js
@@ -3,10 +3,10 @@ import { graphql, Link } from "gatsby"
 import styled from "@emotion/styled"
 import { css } from "@emotion/react"
 import Layout from "../components/Layout"
-import ToggleButton from "../components/ToggleButton"
 import {
   ChakraProvider,
   extendTheme,
+  Box,
   Flex,
   Spacer,
   Grid,
@@ -17,6 +17,7 @@ import {
   Center,
   Text,
   Tag,
+  Tooltip,
   Slider,
   SliderTrack,
   SliderFilledTrack,
@@ -27,11 +28,38 @@ import {
   FormErrorMessage,
   FormHelperText,
 } from "@chakra-ui/react"
+import ToggleButton from "../components/ToggleButton"
 import Wizard from "../components/Wizard"
+import { RiGithubFill as IGithub } from "react-icons/ri"
+
+export const query = graphql`
+  {
+    allAirtable(
+      filter: { table: { eq: "Sample Type" } }
+      sort: { order: ASC, fields: rowIndex }
+    ) {
+      nodes {
+        data {
+          Name
+          Unit
+          Available_Data_Types {
+            data {
+              Name
+            }
+            recordId
+          }
+          Featured
+        }
+        recordId
+      }
+    }
+  }
+`
 
 const Catalog = ({ data }) => {
   const sampleTypes = data.allAirtable.nodes.map((item) => ({
     name: item.data.Name,
+    unit: item.data.Unit,
     recordId: item.recordId,
     selected: false,
     amount: 0,
@@ -114,12 +142,13 @@ const Catalog = ({ data }) => {
                         <div>
                           <FormControl id="amount">
                             <Grid
-                              templateColumns="1fr minmax(2rem, auto)"
+                              templateColumns="1fr minmax(3rem, auto)"
                               mt={4}
                               align="center"
                               gap={4}
                             >
                               <Slider
+                                max={999}
                                 value={item.amount}
                                 onChange={(value) =>
                                   setState((prevState) => {
@@ -135,7 +164,10 @@ const Catalog = ({ data }) => {
                                 <SliderThumb />
                               </Slider>
                               <FormHelperText m={0} textAlign="right">
-                                {item.amount} mm<sup>2</sup>
+                                {item.amount}{" "}
+                                <Tooltip label="microliter">
+                                  {item.unit}
+                                </Tooltip>
                               </FormHelperText>
                             </Grid>
                           </FormControl>
@@ -155,23 +187,20 @@ const Catalog = ({ data }) => {
                     ?.filter((item, i) => item.amount > 0)
                     ?.map((item, i) => (
                       <Row key={item.recordId}>
-                        <div>
+                        <Box>
                           <Heading as="h2" mt={4}>
                             {item.name}
                           </Heading>
                           <Text color="gray.500">
-                            {item.amount} mm<sup>2</sup>
+                            {item.amount}{" "}
+                            <Tooltip label="microliter">{item.unit}</Tooltip>
                           </Text>
-                        </div>
-                        <Wrap mt={4}>
+                        </Box>
+                        <Box mt={4}>
                           {item?.dataTypes?.map((item, i) => {
-                            return (
-                              <WrapItem>
-                                <Tag>{item.name}</Tag>
-                              </WrapItem>
-                            )
+                            return <Text m={0}>{item.name}</Text>
                           })}
-                        </Wrap>
+                        </Box>
                       </Row>
                     ))}
                 </article>
@@ -192,33 +221,30 @@ const Catalog = ({ data }) => {
           </Container>
         </Center>
       </Layout>
+      <Footer>
+        <div>
+          Demo software
+          <a
+            href="https://airtable.com/shrKBMP5HUmm97PgO"
+            target="_blank"
+            rel="noopener"
+          >
+            Feedback & Contribute
+          </a>
+          <a href="https://github.com/researchtothepeople/rttp-jam">
+            <IGithub
+              aria-label="View source on Github"
+              target="_blank"
+              rel="noopener"
+            />
+          </a>
+        </div>
+      </Footer>
     </ChakraProvider>
   )
 }
 
 export default Catalog
-export const query = graphql`
-  {
-    allAirtable(
-      filter: { table: { eq: "Sample Type" } }
-      sort: { order: ASC, fields: rowIndex }
-    ) {
-      nodes {
-        data {
-          Name
-          Available_Data_Types {
-            data {
-              Name
-            }
-            recordId
-          }
-          Featured
-        }
-        recordId
-      }
-    }
-  }
-`
 
 const theme = extendTheme({
   fonts: {
@@ -240,4 +266,23 @@ const Row = styled.div`
   }
   display: grid;
   grid-template-columns: 1fr 2fr;
+`
+
+const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  font-size: 0.875rem;
+  padding-left: 1em;
+  background-color: var(--BG0);
+  border-top-left-radius: 0.5em;
+  > div {
+    margin: 0;
+    padding: 0.75em 1.25em 0.75em 0;
+    display: flex;
+    align-items: center;
+    gap: 0.75em;
+    border-top: 1px solid var(--BG2);
+    color: var(--FG2);
+  }
 `
